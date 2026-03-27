999d296776448b0a9e4e39b29368352976bb6ca6
Michael Nogh <michaelnogh@gmail.com>
2026-03-27 15:26:19 +0300

    Chaening Helek 7

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # app.py
 from utils import add_entry, read_entries, delete_last_entry
 
-add_entry("A: refining update")
+add_entry("A iteration")
 
 delete_last_entry()
-add_entry("B iteration")
+add_entry("B: replaced deleted entry")
 
 for line in read_entries():
     print(line)
     
+    
+    
+ 
